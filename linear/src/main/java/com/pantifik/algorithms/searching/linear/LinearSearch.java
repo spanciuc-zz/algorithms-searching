746d576783d663dc041e6598fb27ec6ac0724d1b
spanciuc <25828627+spanciuc@users.noreply.github.com>
2022-05-07 13:46:27 +0200

    Added javadoc

--- linear/src/main/java/com/pantifik/algorithms/searching/linear/LinearSearch.java
+++ linear/src/main/java/com/pantifik/algorithms/searching/linear/LinearSearch.java
@@ -8,6 +8,24 @@ public class LinearSearch {
 
   private LinearSearch() {}
 
+  /**
+   * Performs a linear search of the element in the given array.
+   * <p>
+   * Complexity O(n).
+   *
+   * @param array
+   *     the array to search in.
+   * @param obj
+   *     the object to search.
+   * @param <T>
+   *     the type of the elements in the array.
+   * @param <S>
+   *     the type of the elements to search.
+   * @return the OptionalInt with the index of the element if found, otherwise empty OptionalInt.
+   *
+   * @throws NullPointerException
+   *     if the given array is null.
+   */
   public static <T, S extends T> OptionalInt search(T[] array, S obj) {
     Objects.requireNonNull(array);
     return IntStream.range(0, array.length)
